a80c6d71e162d3477275a1b0dc777bc023fb968d
Julianus Pfeuffer <8102638+jpfeuffer@users.noreply.github.com>
2026-06-09 16:48:16 +0200

    nop. rerun ci

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 # https://github.com/conda-forge/pysyntect-feedstock/
 set -ex
 
-# Build package via pip
+# Build package via pip.
 $PYTHON -m pip install . -vv --no-deps --no-build-isolation
 
 # Bundle all downstream library licenses
